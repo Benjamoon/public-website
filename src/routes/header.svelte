8064374d953ae6cdb4e3165f30d12ab5a803a6cc
Ben <ben@h0st.uk>
2024-06-14 17:59:58 +0100

    Update links to show active status

--- src/routes/header.svelte
+++ src/routes/header.svelte
@@ -1,4 +1,5 @@
 <script lang="ts">
+	import { page } from '$app/stores'
 	import * as config from '$lib/config'
 	import Toggle from './toggle.svelte'
 </script>
@@ -10,13 +11,13 @@
 
 	<ul class="links">
 		<li>
-			<a href="/blog">Blog</a>
+			<a href="/blog" class:active={$page.url.pathname.startsWith('/blog')}>Blog</a>
 		</li>
 		<li>
-			<a href="/about">About</a>
+			<a href="/about" class:active={$page.url.pathname.startsWith('/about')}>About</a>
 		</li>
 		<li>
-			<a href="/contact">Contact</a>
+			<a href="/contact" class:active={$page.url.pathname.startsWith('/contact')}>Contact</a>
 		</li>
 		<li>
 			<a href="/rss.xml" target="_blank">RSS</a>
@@ -38,6 +39,11 @@
     a {
         color: inherit;
         text-decoration: none;
+    }
+
+    a.active {
+        font-weight: bold;
+        text-decoration: underline;
     }
 
     @media (min-width: 768px) {
